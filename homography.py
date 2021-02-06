961ad465107ee8da7c4a47df6dcd08bf311565a8
Kirk Eaton <contact@kirkeaton.ca>
2021-02-06 17:34:46 -0500

    feat: Upgrade to Python 3

--- homography.py
+++ homography.py
@@ -1,26 +1,29 @@
 import numpy
 
+
 def normalize(points):
     for row in points:
         row /= points[-1]
     return points
 
+
 def make_homog(points):
     return numpy.vstack((points, numpy.ones((1, points.shape[1]))))
 
+
 def H_from_points(fp, tp):
-    '''Find H such that H * fp = tp.
-    
+    """Find H such that H * fp = tp.
+
     H has eight degrees of freedom, so this needs at least 4 points in fp and tp.
-    '''
+    """
     if fp.shape != tp.shape:
-        raise RuntimeError('number of points do not match')
+        raise RuntimeError("number of points do not match")
 
     # condition:
     # -from
     m = numpy.mean(fp[:2], axis=1)
     maxstd = numpy.max(numpy.std(fp[:2], axis=1)) + 1e-9
-    C1 = numpy.diag([1/maxstd, 1/maxstd, 1])
+    C1 = numpy.diag([1 / maxstd, 1 / maxstd, 1])
     C1[0, 2] = -m[0] / maxstd
     C1[1, 2] = -m[1] / maxstd
     fp = numpy.dot(C1, fp)
@@ -28,7 +31,7 @@ def H_from_points(fp, tp):
     # -to
     m = numpy.mean(tp[:2], axis=1)
     maxstd = numpy.max(numpy.std(tp[:2], axis=1)) + 1e-9
-    C2 = numpy.diag([1/maxstd, 1/maxstd, 1])
+    C2 = numpy.diag([1 / maxstd, 1 / maxstd, 1])
     C2[0, 2] = -m[0] / maxstd
     C2[1, 2] = -m[1] / maxstd
     tp = numpy.dot(C2, tp)
@@ -36,10 +39,28 @@ def H_from_points(fp, tp):
     correspondences_count = fp.shape[1]
     A = numpy.zeros((2 * correspondences_count, 9))
     for i in range(correspondences_count):
-        A[2 * i    ] = [-fp[0][i], -fp[1][i], -1, 0, 0, 0,
-                        tp[0][i]  * fp[0][i], tp[0][i] * fp[1][i], tp[0][i]]
-        A[2 * i + 1] = [0, 0, 0, -fp[0][i], -fp[1][i], -1,
-                        tp[1][i]  * fp[0][i], tp[1][i] * fp[1][i], tp[1][i]]
+        A[2 * i] = [
+            -fp[0][i],
+            -fp[1][i],
+            -1,
+            0,
+            0,
+            0,
+            tp[0][i] * fp[0][i],
+            tp[0][i] * fp[1][i],
+            tp[0][i],
+        ]
+        A[2 * i + 1] = [
+            0,
+            0,
+            0,
+            -fp[0][i],
+            -fp[1][i],
+            -1,
+            tp[1][i] * fp[0][i],
+            tp[1][i] * fp[1][i],
+            tp[1][i],
+        ]
 
     U, S, V = numpy.linalg.svd(A)
     H = V[8].reshape((3, 3))
@@ -50,18 +71,18 @@ def H_from_points(fp, tp):
 
 
 def Haffine_from_points(fp, tp):
-    '''Find affine H such that H * fp = tp.
-  
+    """Find affine H such that H * fp = tp.
+
     H has six degrees of freedom, so this needs at least 3 points in fp and tp.
-    '''
+    """
     if fp.shape != tp.shape:
-        raise RuntimeError('number of points do not match')
+        raise RuntimeError("number of points do not match")
 
     # condition:
     # -from
     m = numpy.mean(fp[:2], axis=1)
     maxstd = numpy.max(numpy.std(fp[:2], axis=1)) + 1e-9
-    C1 = numpy.diag([1/maxstd, 1/maxstd, 1])
+    C1 = numpy.diag([1 / maxstd, 1 / maxstd, 1])
     C1[0, 2] = -m[0] / maxstd
     C1[1, 2] = -m[1] / maxstd
     fp_cond = numpy.dot(C1, fp)
@@ -69,7 +90,7 @@ def Haffine_from_points(fp, tp):
     # -to
     m = numpy.mean(tp[:2], axis=1)
     maxstd = numpy.max(numpy.std(tp[:2], axis=1)) + 1e-9
-    C2 = numpy.diag([1/maxstd, 1/maxstd, 1])
+    C2 = numpy.diag([1 / maxstd, 1 / maxstd, 1])
     C2[0, 2] = -m[0] / maxstd
     C2[1, 2] = -m[1] / maxstd
     tp_cond = numpy.dot(C2, tp)
@@ -81,9 +102,9 @@ def Haffine_from_points(fp, tp):
     B = tmp[:2]
     C = tmp[2:4]
 
-    tmp2 = numpy.concatenate((numpy.dot(C, numpy.linalg.pinv(B)),
-                                numpy.zeros((2, 1))),
-                            axis=1)
+    tmp2 = numpy.concatenate(
+        (numpy.dot(C, numpy.linalg.pinv(B)), numpy.zeros((2, 1))), axis=1
+    )
     H = numpy.vstack((tmp2, [0, 0, 1]))
 
     # decondition
@@ -111,10 +132,12 @@ class RansacModel(object):
 
 def H_from_ransac(fp, tp, model, maxiter=1000, match_threshold=10):
     import ransac
+
     data = numpy.vstack((fp, tp))
-    H, ransac_data = ransac.ransac(data.T, model, 4, maxiter, match_threshold, 10,
-                                    return_all=True)
-    return H, ransac_data['inliers']
+    H, ransac_data = ransac.ransac(
+        data.T, model, 4, maxiter, match_threshold, 10, return_all=True
+    )
+    return H, ransac_data["inliers"]
 
 
 class AffineRansacModel(object):
@@ -130,14 +153,16 @@ class AffineRansacModel(object):
         tp = data[3:]
 
         fp_transformed = numpy.dot(H, fp)
-        #normalize(fp_transformed)
+        # normalize(fp_transformed)
 
         return numpy.sqrt(numpy.sum((tp - fp_transformed) ** 2, axis=0))
 
 
 def Haffine_from_ransac(fp, tp, model, maxiter=1000, match_threshold=10):
     import ransac
+
     data = numpy.vstack((fp, tp))
-    H, ransac_data = ransac.ransac(data.T, model, 3, maxiter, match_threshold, 7,
-                                    return_all=True)
-    return H, ransac_data['inliers']
+    H, ransac_data = ransac.ransac(
+        data.T, model, 3, maxiter, match_threshold, 7, return_all=True
+    )
+    return H, ransac_data["inliers"]
